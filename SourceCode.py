a2374354f0fdcb645daa60c309097bf95ee33492
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2023-12-01 16:30:17 +0700

    insialisasi classifier yang digunakan

--- SourceCode.py
+++ SourceCode.py
@@ -19,3 +19,17 @@ X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'],
 tfidf_vectorizer = TfidfVectorizer()
 X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
 X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+# Initialize classifiers
+classifiers = {
+    'Naive Bayes': MultinomialNB(),
+    'Logistic Regression': LogisticRegression(),
+    'Decision Tree': DecisionTreeClassifier(),
+    'SVM': SVC()
+}
+
+# Train and predict using each classifier
+predictions = {}
+for model, classifier in classifiers.items():
+    classifier.fit(X_train_tfidf, y_train)
+    predictions[model] = classifier.predict(X_test_tfidf)
